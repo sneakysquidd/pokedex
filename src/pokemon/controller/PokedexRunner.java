4f2595075e9adf84a33f186ebf1e685ef619a15a
_gavbob <gsct2002@gmail.com>
2019-02-04 13:14:29 -0700

    implemented controller and runner changes for GUI

---
+++ src/pokemon/controller/PokedexRunner.java
@@ -0,0 +1,11 @@
+package pokemon.controller;
+
+public class PokedexRunner
+{
+	public static void main  ( String args[])
+	{
+		PokedexController app = new PokedexController();
+		app.start();
+	}
+		
+}
