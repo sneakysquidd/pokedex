b53b0b0a9e151084c1c22c74d44482234ee93006
_gavbob <gsct2002@gmail.com>
2019-02-04 13:08:45 -0700

    Added labels and textfields

--- src/pokemon/view/PokedexFrame.java
+++ src/pokemon/view/PokedexFrame.java
@@ -1,6 +1,31 @@
 package pokemon.view;
 
-public class PokedexFrame
-{
+import javax.swing.JFrame;
 
+import pokemon.view.PokedexPanel;
+import pokemon.controller.PokedexController;
+
+public class PokedexFrame extends JFrame
+{
+	private PokedexController appController;
+	private PokedexPanel appPanel;
+	
+	public PokedexFrame(PokedexController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		this.appPanel = new PokedexPanel(appController);
+	
+		setupFrame();
+	}
+	
+	public void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(800, 1000);
+		this.setTitle("Pokedex");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -1,6 +1,53 @@
 package pokemon.view;
 
-public class PokedexPanel
-{
+import javax.swing.SpringLayout;
+import javax.swing.*;
 
+import pokemon.controller.PokedexController;
+
+public class PokedexPanel extends JPanel
+{
+	private SpringLayout appLayout;
+	private PokedexController appController;
+	private JTextField WigglytuffText;
+	private JLabel WifflytuffLabel;
+	private JTextField DewgongText;
+	private JLabel DewgongLabel;
+	private JTextField DrugongText;
+	private JLabel DrugongLabel;
+	private JTextField PoliwhirlText;
+	private JLabel PoliwhirlLabel;
+	private JTextField ZangooseText;
+	private JLabel ZangooseLabel;
+	private JTextField QuagsireText;
+	private JLabel QuagsireLabel;
+	
+	public PokedexPanel(PokedexController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		appLayout = new SpringLayout();
+		
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
